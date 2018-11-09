fd143f21cf5c71b8dba9ffaf2e284afce9c4de09
Jason Belt <belt@ksu.edu>
2018-11-09 11:39:43 -0600

    sort types

--- models/smaccmpilot-tk1/act/includes/tb_smaccmpilot_tk1_types.h
+++ models/smaccmpilot-tk1/act/includes/tb_smaccmpilot_tk1_types.h
@@ -8,26 +8,6 @@
 #define TB_MONITOR_READ_ACCESS 111
 #define TB_MONITOR_WRITE_ACCESS 222
 
-typedef uint8_t SMACCM_DATA__GIDL [80];
-
-typedef
-  struct tb_SMACCM_DATA__GIDL_container {
-    SMACCM_DATA__GIDL f;
-  } tb_SMACCM_DATA__GIDL_container;
-
-typedef uint8_t SMACCM_DATA__UART_Buffer [255];
-
-typedef
-  struct tb_SMACCM_DATA__UART_Buffer_container {
-    SMACCM_DATA__UART_Buffer f;
-  } tb_SMACCM_DATA__UART_Buffer_container;
-
-typedef
-  struct SMACCM_DATA__UART_Packet_i {
-    SMACCM_DATA__UART_Buffer buf;
-    int32_t buf_len;
-  } SMACCM_DATA__UART_Packet_i;
-
 typedef uint8_t SMACCM_DATA__CAN_Buffer [8];
 
 typedef
@@ -49,5 +29,25 @@ typedef
     uint16_t top;
     uint16_t bottom;
   } SMACCM_DATA__Camera_Bounding_Box_i;
+
+typedef uint8_t SMACCM_DATA__GIDL [80];
+
+typedef
+  struct tb_SMACCM_DATA__GIDL_container {
+    SMACCM_DATA__GIDL f;
+  } tb_SMACCM_DATA__GIDL_container;
+
+typedef uint8_t SMACCM_DATA__UART_Buffer [255];
+
+typedef
+  struct tb_SMACCM_DATA__UART_Buffer_container {
+    SMACCM_DATA__UART_Buffer f;
+  } tb_SMACCM_DATA__UART_Buffer_container;
+
+typedef
+  struct SMACCM_DATA__UART_Packet_i {
+    SMACCM_DATA__UART_Buffer buf;
+    int32_t buf_len;
+  } SMACCM_DATA__UART_Packet_i;
 
 #endif // __TB_AADL_tb_smaccmpilot_tk1_types__H
